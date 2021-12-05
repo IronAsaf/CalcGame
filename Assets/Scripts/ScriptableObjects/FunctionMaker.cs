ab7a204b33116e441ec99accbf73055be3ffaa8e
IronAsaf <asafdangoor@gmail.com>
2021-12-05 18:17:52 +0200

    Setup for scriptables

---
+++ Assets/Scripts/ScriptableObjects/FunctionMaker.cs
@@ -0,0 +1,11 @@
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace ScriptableObjects
+{
+    [CreateAssetMenu(menuName = "CalcGame/Function Data", fileName = "FunctionData_00")]
+    public class FunctionMaker : SerializedScriptableObject
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/LevelMaker.cs
@@ -0,0 +1,11 @@
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace ScriptableObjects
+{
+    [CreateAssetMenu(menuName = "CalcGame/Level Data", fileName = "LevelData_00")]
+    public class LevelMaker : SerializedScriptableObject
+    {
+    
+    }
+}
