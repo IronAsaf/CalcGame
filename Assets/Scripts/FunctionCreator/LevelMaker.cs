4501320c690437ae71262e5b6fb048a5caed941b
IronAsaf <asafdangoor@gmail.com>
2022-02-01 00:32:50 +0200

    Level adjustments for Reordfer and replay

--- Assets/Scripts/FunctionCreator/LevelMaker.cs
+++ Assets/Scripts/FunctionCreator/LevelMaker.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
@@ -11,7 +12,36 @@ namespace FunctionCreator
         [SerializeField] private int difficulty;
         public List<FunctionMaker> functionsForLevelList;
         public FunctionMaker bottomFunction;
-        public float speedIncreasePerHit = 0.025f;
+        public float speedIncreasePerHit = 0.3f;
         public float lineFunctionYPos = 0;
+        public int minimumAmountOfScoreToWin = 5000;
+
+        private void Awake()
+        {
+            ReorderList();
+        }
+
+        private void ReorderList()
+        {
+            foreach (var func in functionsForLevelList)
+            {
+                if (func.name.Equals(bottomFunction.name))
+                {
+                    var baser=func;
+                    functionsForLevelList.Remove(func);
+                    functionsForLevelList.Insert(functionsForLevelList.Count/2,baser);
+                    break;
+                }
+            }
+            
+        }
+
+        public void ResetLevel()
+        {
+            //generate a new bottom function
+            var rand = Random.Range(0, functionsForLevelList.Count);
+            bottomFunction = functionsForLevelList[rand];
+            ReorderList();
+        }
     }
 }
